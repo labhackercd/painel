d4dce8bde504929dd0378ff2bc6b8c62c6c01c0b
Petterson Alves <pettalves@gmail.com>
2018-07-25 15:01:15 -0300

    Improve graph (#8)

--- src/static/js/chart.js
+++ src/static/js/chart.js
@@ -16,13 +16,16 @@ if (category_id){
 
 $.getJSON('/areachart' + param, function(data) {
   var datasets = new Array();
-  $.each(data.categories, function(key, value){ 
+  $.each(data.categories, function(key, value){
+    var color = getRandomColor();
     datasets.push({
       label: key,
       data: value,
-      borderColor: getRandomColor(),
-      borderWidth: 1,
+      borderColor: color,
+      pointBackgroundColor: color,
+      borderWidth: 2,
       fill: false,
+      backgroundColor: color,
     });
   });
 
@@ -36,7 +39,15 @@ $.getJSON('/areachart' + param, function(data) {
       filler: {
         propagate: true
       }
-    }
+    },
+    legend: {
+      position: 'bottom',
+      labels: {
+        usePointStyle: true,
+        padding: 30,
+        fullWidht: true,
+      },
+    },
   }
 
   // Get context with jQuery - using jQuery's .get() method.
