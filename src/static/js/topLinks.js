318ef435f20cacd5aa882e8bbd8fa77ebd6eb4d1
erivanio <erivanio.vanin@gmail.com>
2018-08-20 12:00:22 -0300

    get meta tags into js

--- src/static/js/topLinks.js
+++ src/static/js/topLinks.js
@@ -5,38 +5,35 @@ function loadTopLinks(params) {
     topLinks.html('');
     if (data.length) {
       $.each(data, function(i, data) {
-        if (data.title) {
-          var title = data.title;
-        } else {
-          var title = '';
-        }
-
-        var element = `
-          <div class="item">
-            <div class="bar js-top-link-bar">
-              <div class="content">
-                <h2>${title}</h2>
-                <a class="js-top-link-href" href="${data.url}" target="_blank">${data.display_url}</a>
-              </div>
-              <div class="mentions">
-                <span class="value">${data.retweets}</span>
-                <span>menções</span>
+        $.get(data.url, function(data) {
+          var title = $(data).find('meta[name=og-title]').attr("content");  
+          var element = `
+            <div class="item">
+              <div class="bar js-top-link-bar">
+                <div class="content">
+                  <h2>${title}</h2>
+                  <a class="js-top-link-href" href="${data.url}" target="_blank">${data.display_url}</a>
+                </div>
+                <div class="mentions">
+                  <span class="value">${data.retweets}</span>
+                  <span>menções</span>
+                </div>
               </div>
             </div>
-          </div>
-        `
-        var element = $(element)
-        element.on('click', '.js-top-link-href', function(e) {
-          window.open($(this).attr('href'), '_blank');
-        });
-        element.on('click', '.js-top-link-bar', {link: data.id, url: data.display_url}, function(e) {
-          localStorage.setItem('link', e.data['link']);
-          addFilterTag('blue', 'link', e.data['url']);
-          loadContainers();
+          `
+          var element = $(element)
+          element.on('click', '.js-top-link-href', function(e) {
+            window.open($(this).attr('href'), '_blank');
+          });
+          element.on('click', '.js-top-link-bar', {link: data.id, url: data.display_url}, function(e) {
+            localStorage.setItem('link', e.data['link']);
+            addFilterTag('blue', 'link', e.data['url']);
+            loadContainers();
 
-          return false;
+            return false;
+          });
+          topLinks.append(element);
         });
-        topLinks.append(element);
       })
     } else {
       topLinks.html(`
